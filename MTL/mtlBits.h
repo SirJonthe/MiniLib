9ba7b96a72782447882ff0e0b79d1dd947800e17
Jonathan Karlsson <er.jonathan.karlsson@hotmail.com>
2015-03-03 23:44:28 +0100

    Added some bit twiddling functions in mtlBits.h

---
+++ MTL/mtlBits.h
@@ -0,0 +1,104 @@
+#ifndef MTL_BITS_H_INCLUDED__
+#define MTL_BITS_H_INCLUDED__
+
+
+inline unsigned long long mtlEncodeMorton2(unsigned int x, unsigned int y)
+{
+	x = (x | (x << 8)) & 0x00FF00FF;
+	x = (x | (x << 4)) & 0x0F0F0F0F;
+	x = (x | (x << 2)) & 0x33333333;
+	x = (x | (x << 1)) & 0x55555555;
+
+	y = (y | (y << 8)) & 0x00FF00FF;
+	y = (y | (y << 4)) & 0x0F0F0F0F;
+	y = (y | (y << 2)) & 0x33333333;
+	y = (y | (y << 1)) & 0x55555555;
+
+	return x | (y << 1);
+}
+
+inline unsigned long long mtlEncodeMorton3(unsigned int x, unsigned int y, unsigned int z)
+{
+	x &= 0x001FFFFF;
+	x = (x | (x << 16)) & 0xFF0000FF;
+	x = (x | (x <<  8)) & 0x0300F00F;
+	x = (x | (x <<  4)) & 0x030C30C3;
+	x = (x | (x <<  2)) & 0x09249249;
+
+	y &= 0x001FFFFF;
+	y = (y | (y << 16)) & 0xFF0000FF;
+	y = (y | (y <<  8)) & 0x0300F00F;
+	y = (y | (y <<  4)) & 0x030C30C3;
+	y = (y | (y <<  2)) & 0x09249249;
+
+	z &= 0x001FFFFF;
+	z = (z | (z << 16)) & 0xFF0000FF;
+	z = (z | (z <<  8)) & 0x0300F00F;
+	z = (z | (z <<  4)) & 0x030C30C3;
+	z = (z | (z <<  2)) & 0x09249249;
+
+	return x | (y << 1) | (z << 2);
+}
+
+inline unsigned int mtlDecodeMorton2X(unsigned long long code)
+{
+	code &= 0x55555555;
+	code = (code | (code >>  1)) & 0x33333333;
+	code = (code | (code >>  2)) & 0x0F0F0F0F;
+	code = (code | (code >>  4)) & 0x00FF00FF;
+	code = (code | (code >>  8)) & 0x0000FFFF;
+	return code;
+}
+
+inline unsigned int mtlDecodeMorton2Y(unsigned long long code)
+{
+	code = (code >> 1) & 0x55555555;
+	code = (code | (code >>  1)) & 0x33333333;
+	code = (code | (code >>  2)) & 0x0F0F0F0F;
+	code = (code | (code >>  4)) & 0x00FF00FF;
+	code = (code | (code >>  8)) & 0x0000FFFF;
+	return code;
+}
+
+inline unsigned int mtlDecodeMorton3X(unsigned long long code)
+{
+	code &= 0x09249249;
+	code = (code | (code >>  2)) & 0x030C30C3;
+	code = (code | (code >>  4)) & 0x0300F00F;
+	code = (code | (code >>  8)) & 0xFF0000FF;
+	code = (code | (code >> 16)) & 0x000003FF;
+	return code;
+}
+
+inline unsigned int mtlDecodeMorton3Y(unsigned long long code)
+{
+	code = (code >> 1) & 0x09249249;
+	code = (code | (code >>  2)) & 0x030C30C3;
+	code = (code | (code >>  4)) & 0x0300F00F;
+	code = (code | (code >>  8)) & 0xFF0000FF;
+	code = (code | (code >> 16)) & 0x000003FF;
+	return code;
+}
+
+inline unsigned int mtlDecodeMorton3Z(unsigned long long code)
+{
+	code = (code >> 2) & 0x09249249;
+	code = (code | (code >>  2)) & 0x030C30C3;
+	code = (code | (code >>  4)) & 0x0300F00F;
+	code = (code | (code >>  8)) & 0xFF0000FF;
+	code = (code | (code >> 16)) & 0x000003FF;
+	return code;
+}
+
+template < typename type_t >
+type_t mtlReadBit(type_t in_bits, unsigned int i) { return in_bits & (1 << i); }
+
+template < typename type_t >
+inline type_t mtlSetBit(unsigned int i, bool state) { return (((type_t)state) << i); }
+template < typename type_t >
+inline type_t mtlSetBit(type_t in_bits, unsigned int i, bool state) { return in_bits & mtlSetBit(i, state); }
+
+template < typename type_t >
+inline type_t mtlToggleBit(type_t in_bits, unsigned int i) { return in_bits ^ (1 << i); }
+
+#endif
