1768c79d131452ff4952ac04f58e0ae85edf9fe1
Jonathan Karlsson <er.jonathan.karlsson@hotmail.com>
2015-03-03 19:39:55 +0100

    Restructured the smart pointer structures, more efficient and easier to use/understand. Removed mtlVersion.h due to unreliability.

--- MTL/mtlPointer.h
+++ MTL/mtlPointer.h
@@ -4,24 +4,33 @@
 #include "mtlList.h"
 
 template < typename type_t >
+class mtlReference;
+
+template < typename type_t >
 class mtlShared
 {
+	friend class mtlReference<type_t>;
+
 private:
-	type_t	*m_obj;
-	int		*m_count;
+	type_t	*m_obj; // delete if count==0
+	int		*m_count; // increment on create, decrement on destroy, only delete if watchers==0
+	int		*m_watchers; // only delete this is watchers==0
+
 public:
 	mtlShared( void );
 	~mtlShared( void );
 	template < typename derived_t >
-	mtlShared(const mtlShared<derived_t> &shared) : m_obj(NULL), m_count(NULL)
+	mtlShared(const mtlShared<derived_t> &shared) : m_obj(NULL), m_count(NULL), m_watchers(NULL)
 	{
 		if (!shared.IsNull()) {
 			m_obj = shared.m_obj;
 			m_count = shared.m_count;
+			m_watchers = shared.m_watchers;
 			++(*m_count);
 		}
 	}
 	mtlShared(const mtlShared<type_t> &shared);
+
 	template < typename derived_t >
 	mtlShared &operator=(const mtlShared<derived_t> &shared)
 	{
@@ -29,30 +38,35 @@ public:
 		// instead, compare m_count address
 		if (m_count != shared.m_count) {
 			Delete();
-			m_obj = shared.m_obj;
+			m_obj = shared.m_obj; // we rely on the compiler to tell us if this is a mismatch
 			m_count = shared.m_count;
+			m_watchers = shared.m_watchers;
 			if (!IsNull()) { ++(*m_count); }
 		}
 		return *this;
 	}
 	mtlShared &operator=(const mtlShared<type_t> &shared);
+
 	type_t *GetShared( void );
 	const type_t *GetShared( void ) const;
 	type_t *operator->( void ) { return GetShared(); }
 	const type_t *operator->( void ) const { return GetShared(); }
+
 	template < typename derived_t >
 	void New( void );
 	void New( void );
 	void Delete( void );
+
 	bool IsNull( void ) const;
-	int GetCount( void ) const;
+	int GetShareCount( void ) const;
+
 	template < typename derived_t >
 	static mtlShared<type_t> Create( void );
 	static mtlShared<type_t> Create( void );
 };
 
 template < typename type_t >
-mtlShared<type_t>::mtlShared( void ) : m_obj(NULL), m_count(NULL)
+mtlShared<type_t>::mtlShared( void ) : m_obj(NULL), m_count(NULL), m_watchers(NULL)
 {
 }
 
@@ -63,17 +77,12 @@ mtlShared<type_t>::~mtlShared( void )
 }
 
 template < typename type_t >
-type_t *mtlShared<type_t>::GetShared( void )
-{
-	return m_obj;
-}
-
-template < typename type_t >
-mtlShared<type_t>::mtlShared(const mtlShared<type_t> &shared) : m_obj(NULL), m_count(NULL)
+mtlShared<type_t>::mtlShared(const mtlShared<type_t> &shared) : m_obj(NULL), m_count(NULL), m_watchers(NULL)
 {
 	if (!shared.IsNull()) {
 		m_obj = shared.m_obj;
 		m_count = shared.m_count;
+		m_watchers = shared.m_watchers;
 		++(*m_count);
 	}
 }
@@ -81,15 +90,20 @@ mtlShared<type_t>::mtlShared(const mtlShared<type_t> &shared) : m_obj(NULL), m_c
 template < typename type_t >
 mtlShared<type_t> &mtlShared<type_t>::operator=(const mtlShared<type_t> &shared)
 {
-	// we can't rely on m_obj having same address despite pointing to same data (not same types)
-	// instead, compare m_count address
-	if (m_count != shared.m_count) {
+	if (m_obj != shared.m_obj) {
 		Delete();
 		m_obj = shared.m_obj;
 		m_count = shared.m_count;
+		m_watchers = shared.m_watchers;
 		if (!IsNull()) { ++(*m_count); }
 	}
 	return *this;
+}
+
+template < typename type_t >
+type_t *mtlShared<type_t>::GetShared( void )
+{
+	return m_obj;
 }
 
 template < typename type_t >
@@ -105,7 +119,9 @@ void mtlShared<type_t>::New( void )
 	Delete();
 	m_obj = new derived_t;
 	m_count = new int;
+	m_watchers = new int;
 	*m_count = 1;
+	*m_watchers = 0;
 }
 
 template < typename type_t >
@@ -121,11 +137,15 @@ void mtlShared<type_t>::Delete( void )
 		--(*m_count);
 		if ((*m_count) == 0) {
 			delete m_obj;
+		}
+		if ((*m_watchers) == 0) {
 			delete m_count;
+			delete m_watchers;
 		}
 	}
 	m_obj = NULL;
 	m_count = NULL;
+	m_watchers = NULL;
 }
 
 template < typename type_t >
@@ -135,7 +155,7 @@ bool mtlShared<type_t>::IsNull( void ) const
 }
 
 template < typename type_t >
-int mtlShared<type_t>::GetCount( void ) const
+int mtlShared<type_t>::GetShareCount( void ) const
 {
 	return m_count != NULL ? *m_count : 0;
 }
@@ -150,184 +170,87 @@ mtlShared<type_t> mtlShared<type_t>::Create( void )
 }
 
 template < typename type_t >
-mtlShared<type_t> mtlShared<type_t>::Create( void )
-{
-	return Create<type_t>();
-}
-
-template < typename type_t >
-class mtlReference;
-
-template < typename type_t >
-class mtlExclusive
-{
-	friend class mtlReference<type_t>;
-private:
-	mtlShared<type_t>								m_obj; // why a mtlShared? because then we can copy-construct internally without duplicating data
-	mtlShared< mtlList< mtlReference<type_t> > >	m_tracker;
-private:
-	mtlExclusive(const mtlExclusive &exclusive);
-	mtlExclusive &operator=(const mtlExclusive &exclusive);
-public:
-	mtlExclusive( void );
-	~mtlExclusive( void );
-	template < typename derived_t >
-	void New( void );
-	void New( void );
-	void Delete( void );
-	template < typename derived_t >
-	static mtlExclusive<type_t> Create( void );
-	static mtlExclusive<type_t> Create( void );
-	bool IsNull( void ) const;
-	type_t *GetExclusive( void );
-	const type_t *GetExclusive( void ) const;
-};
-
-template < typename type_t >
-mtlExclusive<type_t>::mtlExclusive(const mtlExclusive<type_t> &exclusive) : m_obj(exclusive.m_obj), m_tracker(exclusive.m_tracker)
-{
-}
-
-template < typename type_t >
-mtlExclusive<type_t> &mtlExclusive<type_t>::operator=(const mtlExclusive<type_t> &exclusive)
-{
-	if (&exclusive != this) {
-		Delete();
-		m_obj = exclusive.m_obj;
-		m_tracker = exclusive.m_tracker;
-	}
-	return *this;
-}
-
-template < typename type_t >
-template < typename derived_t >
-void mtlExclusive<type_t>::New( void )
-{
-	Delete();
-	m_obj.New<derived_t>();
-	m_tracker.New();
-}
-
-template < typename type_t >
-void mtlExclusive<type_t>::New( void )
-{
-	New<type_t>();
-}
-
-template < typename type_t >
-void mtlExclusive<type_t>::Delete( void )
-{
-	m_obj.Delete();
-	if (m_obj.IsNull() && !m_tracker.IsNull()) {
-		mtlItem< mtlReference<type_t> > *n = m_tracker->GetFirst();
-		while (n != NULL) {
-			n->GetItem().m_reference = NULL;
-			n->GetItem().m_trackerItem = NULL;
-			n = n->GetNext();
-		}
-		m_tracker.Delete();
-	}
-}
-
-template < typename type_t >
-template < typename derived_t >
-mtlExclusive<type_t> mtlExclusive<type_t>::Create( void )
-{
-	mtlExclusive<type_t> o;
-	o.New<derived_t>();
-	return o;
-}
-
-template < typename type_t >
-mtlExclusive<type_t> mtlExclusive<type_t>::Create( void )
-{
-	return Create<type_t>();
-}
-
-template < typename type_t >
-bool mtlExclusive<type_t>::IsNull( void ) const
-{
-	return m_obj.IsNull();
-}
-
-template < typename type_t >
-type_t *mtlExclusive<type_t>::GetExclusive( void )
-{
-	return m_obj.GetShared();
-}
-
-template < typename type_t >
-const type_t *mtlExclusive<type_t>::GetExclusive( void ) const
-{
-	return m_obj.GetShared();
-}
-
-template < typename type_t >
 class mtlReference
 {
-	friend class mtlExclusive<type_t>;
 private:
-	type_t							*m_reference;
-	mtlItem< mtlReference<type_t> >	*m_trackerItem;
+	type_t	*m_obj;
+	int		*m_count; // do nothing with this (delete this if watchers==0 on destroy)
+	int		*m_watchers; // incement this on create, decrement on destroy
+
 public:
 	mtlReference( void );
-	mtlReference(const mtlReference &ref);
-	mtlReference(mtlExclusive<type_t> &excl);
-	mtlReference &operator=(const mtlReference<type_t> &ref);
-	mtlReference &operator=(mtlExclusive<type_t> &ref);
 	~mtlReference( void );
-	void Delete( void );
-	bool IsNull( void ) const;
+
+	template < typename derived_t >
+	mtlReference(const mtlShared<derived_t> &shared) : m_obj(NULL), m_count(NULL), m_watchers(NULL)
+	{
+		if (!shared.IsNull()) {
+			m_obj = shared.m_obj;
+			m_count = shared.m_count;
+			m_watchers = shared.m_watchers;
+			++(*m_watchers);
+		}
+	}
+	mtlReference(const mtlShared<type_t> &shared);
+
+	template < typename derived_t >
+	mtlReference &operator=(const mtlShared<derived_t> &shared)
+	{
+		// we can't rely on m_obj having same address despite pointing to same data (not same types)
+		// instead, compare m_count address
+		if (m_count != shared.m_count) {
+			Delete();
+			m_obj = shared.m_obj; // we rely on the compiler to tell us if this is a mismatch
+			m_count = shared.m_count;
+			m_watchers = shared.m_watchers;
+			if (!IsNull()) { ++(*m_watchers); }
+		}
+		return *this;
+	}
+	mtlReference &operator=(const mtlShared<type_t> &shared);
+
+	template < typename derived_t >
+	mtlReference(const mtlReference<derived_t> &ref) : m_obj(NULL), m_count(NULL), m_watchers(NULL)
+	{
+		if (!ref.IsNull()) {
+			m_obj = ref.m_obj;
+			m_count = ref.m_count;
+			m_watchers = ref.m_watchers;
+			++(*m_watchers);
+		}
+	}
+	mtlReference(const mtlReference<type_t> &ref);
+
+	template < typename derived_t >
+	mtlReference &operator=(const mtlReference<derived_t> &ref)
+	{
+		// we can't rely on m_obj having same address despite pointing to same data (not same types)
+		// instead, compare m_count address
+		if (m_count != ref.m_count) {
+			Delete();
+			m_obj = ref.m_obj; // we rely on the compiler to tell us if this is a mismatch
+			m_count = ref.m_count;
+			m_watchers = ref.m_watchers;
+			if (!IsNull()) { ++(*m_watchers); }
+		}
+		return *this;
+	}
+	mtlReference &operator=(const mtlReference<type_t> &ref);
+
 	type_t *GetReference( void );
 	const type_t *GetReference( void ) const;
+	type_t *operator->( void ) { return GetReference(); }
+	const type_t *operator->( void ) const { return GetReference(); }
+
+	void Delete( void );
+
+	bool IsNull( void ) const;
+	int GetShareCount( void ) const;
 };
 
 template < typename type_t >
-mtlReference<type_t>::mtlReference( void ) : m_reference(NULL), m_trackerItem(NULL)
-{
-}
-
-template < typename type_t >
-mtlReference<type_t>::mtlReference(const mtlReference<type_t> &ref) : m_reference(NULL), m_trackerItem(NULL)
-{
-	if (ref.m_reference != NULL) {
-		*this = *ref.m_reference;
-	}
-}
-
-template < typename type_t >
-mtlReference<type_t>::mtlReference(mtlExclusive<type_t> &excl) : m_reference(excl.m_obj.GetShared()), m_trackerItem(NULL)
-{
-	if (!excl.IsNull()) {
-		excl.m_tracker->AddLast(this);
-		m_trackerItem = excl.m_tracker->GetLast();
-	}
-}
-
-template < typename type_t >
-mtlReference<type_t> &mtlReference<type_t>::operator=(const mtlReference<type_t> &ref)
-{
-	if (&ref != this) {
-		Delete();
-		if (ref.m_reference != NULL) {
-			*this = *ref.m_reference;
-		}
-	}
-	return *this;
-}
-
-template < typename type_t >
-mtlReference<type_t> &mtlReference<type_t>::operator=(mtlExclusive<type_t> &excl)
-{
-	if (excl.m_obj.GetShared() != m_reference) {
-		Delete();
-		m_reference = excl.m_obj.GetShared();
-		if (m_reference != NULL) {
-			excl.m_tracker->AddLast(this);
-			m_trackerItem = excl.m_tracker->GetLast();
-		}
-	}
-}
+mtlReference<type_t>::mtlReference( void ) : m_obj(NULL), m_count(NULL), m_watchers(NULL)
+{}
 
 template < typename type_t >
 mtlReference<type_t>::~mtlReference( void )
@@ -335,33 +258,91 @@ mtlReference<type_t>::~mtlReference( void )
 	Delete();
 }
 
-
 template < typename type_t >
-void mtlReference<type_t>::Delete( void )
+mtlReference<type_t>::mtlReference(const mtlShared<type_t> &shared) : m_obj(NULL), m_count(NULL), m_watchers(NULL)
 {
-	if (m_reference != NULL) {
-		m_reference = NULL;
-		m_trackerItem->Remove();
-		m_trackerItem = NULL;
+	if (!shared.IsNull()) {
+		m_obj = shared.m_obj;
+		m_count = shared.m_count;
+		m_watchers = shared.m_watchers;
+		++(*m_watchers);
 	}
 }
 
 template < typename type_t >
-bool mtlReference<type_t>::IsNull( void ) const
+mtlReference<type_t> &mtlReference<type_t>::operator=(const mtlShared<type_t> &shared)
 {
-	return m_reference != NULL;
+	if (m_obj != shared.m_obj) {
+		Delete();
+		m_obj = shared.m_obj;
+		m_count = shared.m_count;
+		m_watchers = shared.m_watchers;
+		if (!IsNull()) { ++(*m_watchers); }
+	}
+	return *this;
+}
+
+template < typename type_t >
+mtlReference<type_t>::mtlReference(const mtlReference<type_t> &ref) : m_obj(NULL), m_count(NULL), m_watchers(NULL)
+{
+	if (!ref.IsNull()) {
+		m_obj = ref.m_obj;
+		m_count = ref.m_count;
+		m_watchers = ref.m_watchers;
+		++(*m_watchers);
+	}
+}
+
+template < typename type_t >
+mtlReference<type_t> &mtlReference<type_t>::operator=(const mtlReference<type_t> &ref)
+{
+	if (m_obj != ref.m_obj) {
+		Delete();
+		m_obj = ref.m_obj;
+		m_count = ref.m_count;
+		m_watchers = ref.m_watchers;
+		if (!IsNull()) { ++(*m_watchers); }
+	}
+	return *this;
 }
 
 template < typename type_t >
 type_t *mtlReference<type_t>::GetReference( void )
 {
-	return m_reference;
+	return IsNull() ? NULL : m_obj;
 }
 
 template < typename type_t >
 const type_t *mtlReference<type_t>::GetReference( void ) const
 {
-	return m_reference;
+	return IsNull() ? NULL : m_obj;
+}
+
+template < typename type_t >
+void mtlReference<type_t>::Delete( void )
+{
+	if (m_count != NULL) {
+		--(*m_watchers);
+		if (*m_watchers == 0 && *m_count == 0) {
+			delete m_count;
+			delete m_watchers;
+		}
+	}
+	m_obj = NULL;
+	m_count = NULL;
+	m_watchers = NULL;
+}
+
+template < typename type_t >
+bool mtlReference<type_t>::IsNull( void ) const
+{
+	return m_count == NULL || *m_count == 0;
+}
+
+template < typename type_t >
+int mtlReference<type_t>::GetShareCount( void ) const
+{
+	return IsNull() ? 0 : *m_count;
 }
 
 #endif
