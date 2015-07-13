e6cd4df11299d21b8a7c0dc93a90cebec251e860
Jonathan Karlsson <er.jonathan.karlsson@hotmail.com>
2015-07-13 02:16:07 +0200

    Moved projection functions to mglTransform.h

--- MGL/mglTransform.h
+++ MGL/mglTransform.h
@@ -78,4 +78,14 @@ public:
 	//void				Constrain(float x, float y, float z, float rx, float ry, float rz);
 };
 
+inline float mglProject1D(float point, float cam, float dist, float scale = 1.0f)
+{
+	return ((point - cam) * scale) / dist;
+}
+
+inline mmlVector<2> mglProject2D(mmlVector<2> point, mmlVector<2> cam, float dist, float scale = 1.0f)
+{
+	return ((point - cam) * scale) / dist;
+}
+
 #endif
