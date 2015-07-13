8942ba822cedaec31ee95dd6b94037b527145690
Jonathan Karlsson <er.jonathan.karlsson@hotmail.com>
2015-07-13 02:14:30 +0200

    2D projection

--- MGL/mglProject.h
+++ MGL/mglProject.h
@@ -1,9 +1,14 @@
 #ifndef MGL_PROJECT_H_INCLUDED__
 #define MGL_PROJECT_H_INCLUDED__
 
-inline float mglProject1D(float cam, float src, float dist, float scale = 1.0f)
+inline float mglProject1D(float point, float cam, float dist, float scale = 1.0f)
 {
-	return ((src - cam) * scale) / dist;
+	return ((point - cam) * scale) / dist;
+}
+
+inline mmlVector<2> mglProject2D(mmlVector<2> point, mmlVector<2> cam, float dist, float scale = 1.0f)
+{
+	return ((point - cam) * scale) / dist;
 }
 
 #endif // MGL_PROJECT_H_INCLUDED__
