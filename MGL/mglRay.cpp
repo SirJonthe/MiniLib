1866342abcaafd01d7a46fd18e10ebc612d4adf9
Jonathan Karlsson <er.jonathan.karlsson@hotmail.com>
2016-04-10 14:45:44 +0200

    Retooled MPL to use platform dependent wide types.

--- MGL/mglRay.cpp
+++ MGL/mglRay.cpp
@@ -20,9 +20,7 @@ void mglDifferentialAnalyzer3D::SetInitialState(const mglRay3D &p_ray)
 			m_lengths[i] = (m_xyz[i] + 1.0f - p_ray.origin[i]) * m_delta[i];
 		}
 	}
-	//m_side = -1; // this breaks calling GetDistance()
-	//m_side = 0; // this also breaks calling GetDistance() since it will be 0
-	Step();
+	Step(); // Step once to finish initialization (setting m_side to 0 or -1 breaks GetDistance())
 }
 
 void mglDifferentialAnalyzer3D::Step( void )
@@ -58,7 +56,7 @@ void mglDifferentialAnalyzer2D::SetInitialState(const mglRay2D &p_ray)
 		// Reference:
 			// double deltaDistX = sqrt(1 + (rayDirY * rayDirY) / (rayDirX * rayDirX));
 			// double deltaDistY = sqrt(1 + (rayDirX * rayDirX) / (rayDirY * rayDirY));
-		m_delta[i] = sqrt(1.0f + (m_direction[(i+1)&1]*m_direction[(i+1)&1]) / (m_direction[i]*m_direction[i]));
+		m_delta[i] = sqrt(1.0f + (m_direction[(i + 1) & 1] * m_direction[(i + 1) & 1]) / (m_direction[i] * m_direction[i]));
 		if (m_direction[i] < 0.0f) {
 			m_step[i] = -1;
 			m_lengths[i] = (p_ray.origin[i] - m_xyz[i]) * m_delta[i];
@@ -67,9 +65,7 @@ void mglDifferentialAnalyzer2D::SetInitialState(const mglRay2D &p_ray)
 			m_lengths[i] = (m_xyz[i] + 1.0f - p_ray.origin[i]) * m_delta[i];
 		}
 	}
-	//m_side = -1; // this breaks calling GetDistance()
-	//m_side = 0; // this also breaks calling GetDistance() since it will be 0
-	Step();
+	Step(); // Step once to finish initialization (setting m_side to 0 or -1 breaks GetDistance())
 }
 
 void mglDifferentialAnalyzer2D::Step( void )
