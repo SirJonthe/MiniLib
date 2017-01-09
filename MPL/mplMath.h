53f3699e5aa21aa59eca2de2e58c70c2930c8422
Jonathan Karlsson <er.jonathan.karlsson@hotmail.com>
2017-01-09 19:11:02 +0100

    Added some commented out code for future reference.

--- MPL/mplMath.h
+++ MPL/mplMath.h
@@ -15,8 +15,10 @@ namespace mpl
 	//}
 }
 
+//#ifdef MML_MATH_H_INCLUDED__
 // inline mpl::wide_float mmlFloor(const mpl::wide_float &x) { return mpl::Floor(x); }
 // inline mpl::wide_float mmlCeil(const mpl::wide_float &x)  { return mpl::Ceil(x);  }
 // inline mpl::wide_float mmlRound(const mpl::wide_float &x) { return mpl::Round(x); }
+//#endif
 
 #endif // MPL_MATH_H_INCLUDED__
