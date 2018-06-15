c26dacf369c4154e4a40c36ebec67ee6ed835b04
Jonathan Karlsson <er.jonathan.karlsson@hotmail.com>
2018-06-15 23:00:33 +0200

    need to go back on functionality (missing operations)

--- MPL/mplMath.h
+++ MPL/mplMath.h
@@ -10,9 +10,8 @@ inline mpl::wide_int                         mmlMax(const mpl::wide_int &a, cons
 inline mpl::wide_int                         mmlMin(const mpl::wide_int &a, const mpl::wide_int &b)           { return mpl::wide_int::min(a, b); }
 template < int n > inline mpl::wide_fixed<n> mmlMax(const mpl::wide_fixed<n> &a, const mpl::wide_fixed<n> &b) { return mpl::wide_fixed<n>::max(a, b); }
 template < int n > inline mpl::wide_fixed<n> mmlMin(const mpl::wide_fixed<n> &a, const mpl::wide_fixed<n> &b) { return mpl::wide_fixed<n>::min(a, b); }
-inline mpl::wide_float                       mmlWrap(mpl::wide_float value, const mpl::wide_float &max)       { return mpl::wide_float::mov_if_true(mpl::wide_float::mov_if_true(value, max - (-value % max), value
+//inline mpl::wide_float                       mmlWrap(mpl::wide_float value, const mpl::wide_float &max)       { return mpl::wide_float::mov_if_true(mpl::wide_float::mov_if_true(value, max - (-value % max), value < 0), value % max, value > max); }
 //inline mpl::wide_int                         mmlWrap(mpl::wide_int value, const mpl::wide_int &max)           { return mpl::wide_int::mov_if_true(mpl::wide_int::mov_if_true(value, (max + 1) - (-value % max), value < 0), value % (max + 1), value > max); }
-< 0), value % max, value > max); }
 //template < int n > inline mpl::wide_fixed<n> mmlWrap(mpl::wide_fixed<n> value, const mpl::wide_fixed<n> &max) { return *(mpl::wide_fixed<n>*)&(mmlWrap(*(mpl::wide_int*)&value, *(const mpl::wide_int*)&max)); }
 
 inline mpl::wide_int mmlRound(const mpl::wide_float &x) { return mpl::wide_int(x + 0.5f); }
